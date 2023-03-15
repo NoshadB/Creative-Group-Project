34276a2d8c0ea7fa344264e9d69dfb8be426d0c9
JonathanFajobi <ralufajobi@outlook.com>
2023-03-15 17:43:44 +0000

    Added switch between humdity and carbon dioxide

--- Double Led Test.ino
+++ Double Led Test.ino
@@ -2,18 +2,22 @@
 #include <dht11.h>
 #define DHT11PIN 4
 dht11 DHT11;
+
 // #define LED_STRIP1_PIN 1
 #define LED_STRIP2_PIN 2 //turns on long led strip
 #define NUM_LEDS 51
 #define sensor A0
+
+
 int gasLevel = 0;
 CRGB leds[NUM_LEDS];
 const int button1Pin = 7;
 const int button2Pin = 8;
 int button1State = 0;
 int button2State = 0;
-bool airQual;
-bool Humid;
+bool airQuality;
+bool humidity;
+
 
 void setup() {
  Serial.begin(9600);
@@ -22,7 +26,7 @@ void setup() {
  pinMode(button2Pin, INPUT);
  FastLED.addLeds<WS2812B, LED_STRIP2_PIN, GRB>(leds, NUM_LEDS);
  FastLED.addLeds<WS2812B, LED_STRIP2_PIN, GRB>(leds, NUM_LEDS);
- FastLED.setMaxPowerInVoltsAndMilliamps(10, 500);
+ FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);
  FastLED.clear();
  FastLED.show();
 }
@@ -32,45 +36,74 @@ void loop() {
   button2State= digitalRead(button2Pin);
 
 
+if (humidity) {
+  showHumdity();
+} 
+if (airQuality) {
+  showCarbonDioxide();
+}
 
 
 if (button1State == HIGH){
+//yellow button
+  Serial.println("Carbon Doxide");
+  humidity = false; 
+  airQuality = true; 
+}
 
+if (button2State == HIGH){
+  //green button
+  Serial.println("Humidity");
+  humidity = true; 
+  airQuality = false; 
+  }
 
+}
 
+void showHumdity() {
+  int chk = DHT11.read(DHT11PIN);
+  Serial.print("Humidity (%): ");
+  Serial.println((float)DHT11.humidity, 2);
+  float humidityLevel = (float)DHT11.humidity;
+  
+  Serial.print("Temperature   (C): ");
+  Serial.println((float)DHT11.temperature, 2);
 
- gasLevel= 600;
- Serial.println(analogRead(0));
-  leds[5] = CRGB(0,255,0);
- for (int i = 0; i < 10; i++) {
- if(gasLevel<100){
- leds[i] = CRGB(0,255,0);
-  // leds2[i] = CRGB(0,255,0);
- } else if(gasLevel>100 && gasLevel<250){
- leds[i] = CRGB(255,255,0);
-  // leds2[i] = CRGB(255,255,0);
- } else if(gasLevel>250 && gasLevel<500){
- leds[i] = CRGB(255, 120, 0);
-  // leds2[i] = CRGB(255, 120, 0);
-} else{
- leds[i] = CRGB(255,0,0);
-  // leds2[i] = CRGB(255,0,0);
- }
- }
+  Serial.println(analogRead(0));
+
+  for (int i = 0; i < 10; i++) {
+  if(humidityLevel<25){
+  leds[i] = CRGB(125,138,244);
+  } else if(humidityLevel>25 && humidityLevel<70){
+  leds[i] = CRGB(64,83,240);
+  } else if(humidityLevel>=70 && humidityLevel<90){
+  leds[i] = CRGB(0, 0, 255);
+  } 
+  // leds[i] = CRGB(44, 178, 210);
+  // // leds[i] = CHSV(colorBlue, 87, 127);
+}
  FastLED.show();
 
 }
 
-if (button2State == HIGH){
+void showCarbonDioxide(){
+ gasLevel= 600;
+  Serial.println(analogRead(0)+60);
+  for (int i = 0; i < 10; i++) {
+  if(gasLevel<100){
+  leds[i] = CRGB(243,162,173);
 
-   Serial.println();
+  } else if(gasLevel>100 && gasLevel<250){
+  leds[i] = CRGB(252,88,109);
 
-  int chk = DHT11.read(DHT11PIN);
-  Serial.print("Humidity (%): ");
-   Serial.println((float)DHT11.humidity, 2);
+  } else if(gasLevel>250 && gasLevel<500){
+  leds[i] = CRGB(255, 0, 0);
+  } else { 
+  leds[i] = CRGB(255,0,0);
 
-  Serial.print("Temperature   (C): ");
-  Serial.println((float)DHT11.temperature, 2);
-  delay(2000);
-}
+  }
+// int colorPink= CRGB(155,50,50);
+// leds[i] = CHSV(colorPink, 87, 127);
+ }
+ FastLED.show();
 }
